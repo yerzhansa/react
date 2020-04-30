e076d9d44495c75666ad3fd187a07b52dd90a0e4
Yerzhan Sagyt <erzhan.sagyt@gmail.com>
2020-05-01 00:06:49 +0600

    change Chat FC to Class Components

--- src/components/Chat/Chat.tsx
+++ src/components/Chat/Chat.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, Component } from 'react'
 import { ChatList } from '../ChatList'
 import { MessageList } from '../MessageList'
 import chats from '../../mocks/chat/chatList.json'
@@ -6,27 +6,39 @@ import allMessages from '../../mocks/chat/messageList.json'
 import { IMessage } from '../MessageItem/types'
 import s from './styles.module.css'
 
-export const Chat = () => {
-  const [selectedChatId, setSelectedChatId] = useState<number>()
-  const [chatMessages, setChatMessages] = useState<IMessage[]>([])
+interface Props {} // TODO move to types.ts
 
-  useEffect(() => {
-    const m = allMessages.find(({ chatId }) => selectedChatId === chatId)
-    if (m) setChatMessages(m.messages)
-  }, [selectedChatId])
+interface State {
+  // TODO move to types.ts
+  selectedChatId?: number
+  chatMessages: IMessage[]
+}
 
-  const handleClickOnChat = (id: number) => () => {
-    setSelectedChatId(id)
+export class Chat extends Component<Props, State> {
+  constructor(props: Props) {
+    super(props)
+    this.state = { chatMessages: [] }
   }
 
-  return (
-    <div className={s.root}>
-      <ChatList
-        handleClickOnChat={handleClickOnChat}
-        selectedChatId={selectedChatId}
-        chats={chats}
-      />
-      <MessageList messages={chatMessages} />
-    </div>
-  )
+  handleClickCreator = (id: number) => {
+    return () => {
+      const m = allMessages.find(({ chatId }) => id === chatId)
+      if (m) this.setState({ chatMessages: m.messages, selectedChatId: id })
+    }
+  }
+
+  render(): React.ReactNode {
+    const { selectedChatId, chatMessages } = this.state
+
+    return (
+      <div className={s.root}>
+        <ChatList
+          handleClickOnChat={this.handleClickCreator}
+          selectedChatId={selectedChatId}
+          chats={chats}
+        />
+        <MessageList messages={chatMessages} />
+      </div>
+    )
+  }
 }
